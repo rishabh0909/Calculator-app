7604946caeb2c079031204518bf480415460e645
Rishabh Sharma <55099387+rishabh0909@users.noreply.github.com>
2020-03-12 14:26:15 +0530

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,22 @@
+window.addEventListener('load',bindEvents);
+function bindEvents(){
+var buttonArray = document.querySelectorAll('button');
+console.log('Button Array is ',buttonArray);
+document.querySelector('#clear').addEventListener('click',()=>{
+    document.querySelector('#txt').value='';
+    document.querySelector('#txt').focus();
+});
+document.querySelector('#equal').addEventListener('click',compute);
+buttonArray.forEach(button=>{
+    if(button.id!='equal' && button.id!='clear'){
+    button.addEventListener('click',takeValue);
+    }
+});
+}
+function takeValue(){
+    
+    document.querySelector('#txt').value +=  this.innerText;
+}
+function compute(){
+    document.querySelector('#txt').value = eval(document.querySelector('#txt').value);
+}
